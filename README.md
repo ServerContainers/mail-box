d4b0b53e0530a62b355c3f7be225d4323dde0b15
MarvAmBass <marvambass@gmail.com>
2017-07-31 06:17:41 +0000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,199 @@
-# mail-box
-Alpine based Postfix/Dovecot/Sieve Server to store mails and handle users (MySQL Backend needed)
+# Docker Mail Box Postfix/Dovecot/Sieve (servercontainers/mail-box)
+_maintained by ServerContainers_
+
+[FAQ - All you need to know about the servercontainers Containers](https://marvin.im/docker-faq-all-you-need-to-know-about-the-marvambass-containers/)
+
+## What is it
+
+This Dockerfile (available as ___servercontainers/mail-box___) gives you a dovecot and postfix installation is meant to store mails, handle authentication and users and is based on the famous [Wordaround.org - ISP Mail Tutorials](https://workaround.org/ispmail)
+
+It's based on the [_/alpine](https://registry.hub.docker.com/_/alpine/) Image
+
+View in Docker Registry [servercontainers/mail-box](https://registry.hub.docker.com/u/servercontainers/mail-box/)
+
+View in GitHub [ServerContainers/mail-box](https://github.com/ServerContainers/mail-box)
+
+__It supports B-CRYPT!!!__
+Since it uses alpine as baseimage, it's able to support BCrypt password hashing! :)
+
+All the user backend SQL Statements can be modified. By default they use the default isp mail statements and database scheme.
+
+## Environment variables
+
+__OFFICIAL USER DATABASE CONFIGURATION ENVIRONMENT VARIABLES__
+
+- DEFAULT_PASS_SCHEME
+    - dovecot default_pass_scheme for the password hashes (see dovecot manual)
+    - default: _BLF-CRYPT_
+
+- SQL_VIRTUAL_MAILBOX_DOMAIN
+    - default: _SELECT 1 FROM virtual_domains WHERE name='%s'_
+- SQL_VIRTUAL_MAILBOX_MAPS
+    - default: _SELECT 1 FROM virtual_users WHERE email='%s'"_
+- SQL_VIRTUAL_ALIAS_MAPS
+    - default: _SELECT destination FROM virtual_aliases WHERE source='%s'"_
+- SQL_EMAIL_TO_EMAIL
+    - default: _SELECT email FROM virtual_users WHERE email='%s'"_
+
+- SQL_DOVECOT_PASSWORD_QUERY
+    - default: _SELECT email as user, password FROM virtual_users WHERE email='%u';"_
+
+__OFFICIAL DATABASE ENVIRONMENT VARIABLES__
+- MYSQL_USER__
+    - no default - if null it won't start
+- MYSQL_PASSWORD
+    - no default - if null it won't start
+- MYSQL_HOST
+    - default: _mysql_
+- MYSQL_PORT
+    - default: _3306_ - if you use a different mysql port change it
+- MYSQL\_DBNAME__
+    - default: _mailserver_
+
+__OFFICIAL MAIL ENVIRONMENT VARIABLES__
+
+- MAIL_POSTMASTER_ADDRESS
+    - the address to reach the postmaster (maybe you)
+
+- MAIL_FQDN
+    - specify the mailserver name - only add FQDN not a hostname!
+    - e.g. _my.mailserver.example.com_
+- POSTFIX_SMTPD_BANNER
+    - alter the SMTPD Banner of postfix e.g. _mailserver.example.local ESMTP_
+
+- POSTFIX_MYDESTINATION
+    - specify the domains which this mail-box handles
+
+- AUTO_TRUST_NETWORKS
+    - add all networks this container is connected to and trust them to send mails
+    - _set to any value to enable_
+- ADDITIONAL_MYNETWORKS
+    - add this specific network to the automatically trusted onces
+- MYNETWORKS
+    - ignore all auto configured _mynetworks_ and replace them with this value
+    - _overwrites networks specified in ADDITIONAL_MYNETWORKS_
+
+- RELAYHOST
+    - sets postfix relayhost - please take a look at the official documentation
+    - _The form enclosed with [] eliminates DNS MX lookups. Don't worry if you don't know what that means. Just be sure to specify the [] around the mailhub hostname that your ISP gave to you, otherwise mail may be mis-delivered._
+
+- POSTFIX_SSL_OUT_CERT
+    - path to SSL Client certificate (outgoing connections)
+    - default: _/etc/postfix/tls/client.crt_
+- POSTFIX_SSL_OUT_KEY
+    - path to SSL Client key (outgoing connections)
+    - default: _/etc/postfix/tls/client.key_
+- POSTFIX_SSL_OUT_SECURITY_LEVEL
+    - SSL security level for outgoing connections
+    - default: _may_
+
+- POSTFIX_SSL_IN_CERT
+    - path to SSL Cert/Bundle (incoming connections)
+    - default: _/etc/postfix/tls/bundle.crt_
+- POSTFIX_SSL_IN_KEY
+    - path to SSL Cert key (incoming connections)
+    - default: _/etc/postfix/tls/cert.key_
+- POSTFIX_SSL_IN_SECURITY_LEVEL
+    - SSL security level for incoming connections
+    - default: _may_
+
+- POSTFIX_QUEUE_LIFETIME_BOUNCE
+    - The  maximal  time  a  BOUNCE MESSAGE is queued before it is considered undeliverable
+    - By default, this is the same as the queue life time for regular mail
+- POSTFIX_QUEUE_LIFETIME_MAX
+    - maximum lifetime of regular (non bounce) messages
+
+__HIGH PRIORITY ENVIRONMENT VARIABLE__
+
+the following variable/s are only if you have some specific settings you need.
+They help you overwrite everything after the config was generated.
+If you can update your setting with the variables from above, it is strongly recommended to use them!
+
+_some characters might brake your configuration!_
+
+- POSTFIX_RAW_CONFIG_<POSTFIX_SETTING_NAME>
+    - set/edit all configurations in /etc/postfix/main.cf using the POSTFIX_RAW_CONFIG_ followed by the setting name
+
+_for example: to set_ ___mynetworks_style = subnet___ _just add a environment variable_ ___POSTFIX_RAW_CONFIG_MYNETWORKS_STYLE=subnet___
+
+## Volumes
+
+- /etc/postfix/tls
+    - this is where the container looks for:
+        - dh1024.pem (to overwrite the one generated at container build)
+        - dh512.pem (to overwrite the one generated at container build)
+        - rootCA.crt (to check valid client certificates against)
+        - client.crt (outgoing SSL Client cert)
+        - client.key (outgoing SSL Client key)
+        - bundle.crt (incoming SSL Server cert/bundle)
+        - cert.key (incoming SSL Server key)
+- /etc/postfix/additional
+    - this is where the container looks for:
+        - transport (postfix transport text-file - without been postmaped)
+        - header_checks (postfix header_checks regex file)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+## What is it
+
+This Dockerfile (available as ___servercontainers/isp-mail___) gives you a dovecot and postfix installation
+based on the [Wordaround.org - ISP Mail Tutorials](https://workaround.org/ispmail)
+
+It's based on the [_/ubuntu:14.04](https://registry.hub.docker.com/_/ubuntu/) Image
+
+View in Docker Registry [servercontainers/isp-mail](https://registry.hub.docker.com/u/servercontainers/isp-mail/)
+
+View in GitHub [ServerContainers/isp-mail](https://github.com/ServerContainers/isp-mail)
+
+## Environment variables and defaults
+
+### For Headless installation required
+
+Mail Database Settings
+
+* __MAIL\_MYSQL\_USER__
+ * no default - if null it won't start
+* __MAIL\_MYSQL\_PASSWORD__
+ * no default - if null it won't start
+* __MAIL\_MYSQL\_HOST__
+ * default: _mysql_
+* __MAIL\_MYSQL\_PORT__
+ * default: _3306_ - if you use a different mysql port change it
+* __MAIL\_MYSQL\_DBNAME__
+ * default: _ispmail_
+
+
+## Using the servercontainers/isp-mail Container
+
+First you need a running MySQL Container (you could use: [servercontainers/mysql](https://registry.hub.docker.com/u/servercontainers/mysql/)).
+
+You need to _--link_ your mysql container to servercontainers/isp-mail with the name __mysql__
+
+    docker run \
+      -d \
+      -v /etc/localtime:/etc/localtime:ro \
+      -p 25:25 \
+      -p 143:143 \
+      -p 465:465 \
+      -p 993:993 \
+      -p 4190:4190 \
+      -v $SSL_PATH:/etc/ssl/mailserver \
+      -v $VMAIL_PATH:/var/vmail \
+      --link mysql:mysql \
+      --name ispmail servercontainers/isp-mail
+    MYSQL_HOST=mysql
+    MYSQL_PORT=3306
+    MYSQL_DBNAME="mailserver"
---
+++ scripts/entrypoint.sh
@@ -0,0 +1,356 @@
+#/bin/sh
+
+#
+# can be done on every start...
+#
+chown -R vmail:vmail /var/vmail
+
+AVAILABLE_NETWORKS="127.0.0.0/8"
+if [ ! -z ${AUTO_TRUST_NETWORKS+x} ]; then
+  AVAILABLE_NETWORKS=$(list-available-networks.sh | tr '\n' ',' | sed 's/,$//g')
+  echo ">> trust all available networks: $AVAILABLE_NETWORKS"
+fi
+
+postconf -e "mynetworks=$AVAILABLE_NETWORKS"
+
+if [ ! -z ${ADDITIONAL_MYNETWORKS+x} ]; then
+  echo ">> update mynetworks to: $AVAILABLE_NETWORKS,$ADDITIONAL_MYNETWORKS"
+  postconf -e "mynetworks=$AVAILABLE_NETWORKS,$ADDITIONAL_MYNETWORKS"
+fi
+
+if [ ! -z ${MYNETWORKS+x} ]; then
+  if [ ! -z ${ADDITIONAL_MYNETWORKS+x} ]; then
+    echo ">> Warning ADDITIONAL_MYNETWORKS will be ignored! only $MYNETWORKS will be set!"
+  fi
+  echo ">> update mynetworks to: $MYNETWORKS"
+  postconf -e "mynetworks=$MYNETWORKS"
+fi
+
+
+# only on container creation
+INITIALIZED="/.initialized"
+if [ ! -f "$INITIALIZED" ]; then
+	touch "$INITIALIZED"
+
+  ###
+  # Database Settings
+  ###
+
+  # Variables
+
+  if [ -z ${MYSQL_HOST+x} ]
+  then
+    MYSQL_HOST=mysql
+  fi
+
+  if [ -z ${MYSQL_PORT+x} ]
+  then
+    MYSQL_PORT=3306
+  fi
+  echo ">> using '$MYSQL_HOST:$MYSQL_PORT' as Database Server"
+
+  if [ -z ${MYSQL_DBNAME+x} ]
+  then
+    MYSQL_DBNAME="mailserver"
+  fi
+  echo ">> using '$MYSQL_DBNAME' as Database Name"
+
+  if [ -z ${MYSQL_USER+x} ]
+  then
+    >&2 echo ">> you need to specify a Database User!"
+    exit 2;
+  fi
+
+  if [ -z ${MYSQL_PASSWORD+x} ]
+  then
+    >&2 echo ">> you need to specify a Password for the Database User!"
+    exit 2;
+  fi
+
+  if [ -z ${DEFAULT_PASS_SCHEME+x} ]
+  then
+    DEFAULT_PASS_SCHEME="BLF-CRYPT"
+  fi
+  echo ">> using '$DEFAULT_PASS_SCHEME' as Dovecots default_pass_scheme"
+
+  # Settings
+
+  sed -i "s/\[MYSQL_HOST\]/$MYSQL_HOST/g" /etc/postfix/mysql-virtual-mailbox-domains.cf
+  sed -i "s/\[MYSQL_PORT\]/$MYSQL_PORT/g" /etc/postfix/mysql-virtual-mailbox-domains.cf
+  sed -i "s/\[DB_NAME\]/$MYSQL_DBNAME/g" /etc/postfix/mysql-virtual-mailbox-domains.cf
+  sed -i "s/\[DB_USER\]/$MYSQL_USER/g" /etc/postfix/mysql-virtual-mailbox-domains.cf
+  sed -i "s/\[DB_PASSWORD\]/$MYSQL_PASSWORD/g" /etc/postfix/mysql-virtual-mailbox-domains.cf
+
+  sed -i "s/\[MYSQL_HOST\]/$MYSQL_HOST/g" /etc/postfix/mysql-virtual-mailbox-maps.cf
+  sed -i "s/\[MYSQL_PORT\]/$MYSQL_PORT/g" /etc/postfix/mysql-virtual-mailbox-maps.cf
+  sed -i "s/\[DB_NAME\]/$MYSQL_DBNAME/g" /etc/postfix/mysql-virtual-mailbox-maps.cf
+  sed -i "s/\[DB_USER\]/$MYSQL_USER/g" /etc/postfix/mysql-virtual-mailbox-maps.cf
+  sed -i "s/\[DB_PASSWORD\]/$MYSQL_PASSWORD/g" /etc/postfix/mysql-virtual-mailbox-maps.cf
+
+  sed -i "s/\[MYSQL_HOST\]/$MYSQL_HOST/g" /etc/postfix/mysql-virtual-alias-maps.cf
+  sed -i "s/\[MYSQL_PORT\]/$MYSQL_PORT/g" /etc/postfix/mysql-virtual-alias-maps.cf
+  sed -i "s/\[DB_NAME\]/$MYSQL_DBNAME/g" /etc/postfix/mysql-virtual-alias-maps.cf
+  sed -i "s/\[DB_USER\]/$MYSQL_USER/g" /etc/postfix/mysql-virtual-alias-maps.cf
+  sed -i "s/\[DB_PASSWORD\]/$MYSQL_PASSWORD/g" /etc/postfix/mysql-virtual-alias-maps.cf
+
+  sed -i "s/\[MYSQL_HOST\]/$MYSQL_HOST/g" /etc/postfix/mysql-email2email.cf
+  sed -i "s/\[MYSQL_PORT\]/$MYSQL_PORT/g" /etc/postfix/mysql-email2email.cf
+  sed -i "s/\[DB_NAME\]/$MYSQL_DBNAME/g" /etc/postfix/mysql-email2email.cf
+  sed -i "s/\[DB_USER\]/$MYSQL_USER/g" /etc/postfix/mysql-email2email.cf
+  sed -i "s/\[DB_PASSWORD\]/$MYSQL_PASSWORD/g" /etc/postfix/mysql-email2email.cf
+
+  sed -i "s/\[MYSQL_HOST\]/$MYSQL_HOST/g" /etc/dovecot/dovecot-sql.conf.ext
+  sed -i "s/\[MYSQL_PORT\]/$MYSQL_PORT/g" /etc/dovecot/dovecot-sql.conf.ext
+  sed -i "s/\[DB_NAME\]/$MYSQL_DBNAME/g" /etc/dovecot/dovecot-sql.conf.ext
+  sed -i "s/\[DB_USER\]/$MYSQL_USER/g" /etc/dovecot/dovecot-sql.conf.ext
+  sed -i "s/\[DB_PASSWORD\]/$MYSQL_PASSWORD/g" /etc/dovecot/dovecot-sql.conf.ext
+  sed -i "s/\[DEFAULT_PASS_SCHEME\]/$DEFAULT_PASS_SCHEME/g" /etc/dovecot/dovecot-sql.conf.ext
+
+  #
+  # General
+  #
+
+  if [ -z ${RELAYHOST+x} ]; then
+    echo ">> it is advised to use this container with a relayhost (maybe use servercontainers/mail-gateway)..."
+  else
+    echo ">> setting relayhost to: $RELAYHOST"
+    postconf -e "relayhost=$RELAYHOST"
+  fi
+
+  if [ -z ${MAIL_FQDN+x} ]; then
+    MAIL_FQDN="mailbox.local"
+  fi
+
+  if echo "$MAIL_FQDN" | grep -v '\.'; then
+    MAIL_FQDN="$MAIL_FQDN.local"
+  fi
+  MAIL_FQDN=$(echo "$MAIL_FQDN" | sed 's/[^.0-9a-z\-]//g')
+
+  MAIL_NAME=$(echo "$MAIL_FQDN" | cut -d'.' -f1)
+  MAILDOMAIN=$(echo "$MAIL_FQDN" | cut -d'.' -f2-)
+
+  echo ">> set mail host to: $MAIL_FQDN"
+  sed -i '12a\$myhostname = "'"$MAIL_FQDN"'";\' etc/amavis/conf.d/05-node_id
+  echo "$MAIL_FQDN" > /etc/mailname
+  echo "$MAIL_NAME" > /etc/hostname
+  postconf -e "myhostname=$MAIL_FQDN"
+
+  if [ -z ${POSTFIX_SMTPD_BANNER+x} ]; then
+    POSTFIX_SMTPD_BANNER="$MAIL_FQDN ESMTP"
+  fi
+  echo ">> POSTFIX set smtpd_banner = $POSTFIX_SMTPD_BANNER"
+  postconf -e "smtpd_banner=$POSTFIX_SMTPD_BANNER"
+
+  if [ -z ${MAIL_POSTMASTER_ADDRESS+x} ]
+  then
+    MAIL_POSTMASTER_ADDRESS="postmaster@$MAIL_FQDN"
+  fi
+  echo "postmaster_address = $MAIL_POSTMASTER_ADDRESS" >> /etc/dovecot/conf.d/15-lda.conf
+
+
+  #
+  # SSL
+  #
+
+  if [ -z ${POSTFIX_SSL_OUT_CERT+x} ]; then
+    POSTFIX_SSL_OUT_CERT="/etc/postfix/tls/client.crt"
+  fi
+
+  if [ -z ${POSTFIX_SSL_OUT_KEY+x} ]; then
+    POSTFIX_SSL_OUT_KEY="/etc/postfix/tls/client.key"
+  fi
+
+  if [ -z ${POSTFIX_SSL_OUT_SECURITY_LEVEL+x} ]; then
+    POSTFIX_SSL_OUT_SECURITY_LEVEL="may"
+  fi
+
+  if [[ -f "$POSTFIX_SSL_OUT_CERT" && -f "$POSTFIX_SSL_OUT_KEY" ]]; then
+    echo ">> POSTFIX SSL - enabling outgoing SSL"
+cat <<EOF >> /etc/postfix/main.cf
+##### TLS settings ######
+
+### outgoing connections ###
+# smtp_tls_security_level=encrypt # for secure connections only
+smtp_tls_security_level=$POSTFIX_SSL_OUT_SECURITY_LEVEL
+smtp_tls_cert_file=$POSTFIX_SSL_OUT_CERT
+smtp_tls_key_file=$POSTFIX_SSL_OUT_KEY
+
+smtp_tls_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtp_tls_mandatory_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtp_tls_protocols = !SSLv2 !SSLv3
+smtp_tls_mandatory_protocols = !SSLv2, !SSLv3
+smtp_tls_mandatory_ciphers=high
+
+smtp_tls_session_cache_database = btree:\${data_directory}/smtp_scache
+smtp_tls_loglevel = 1
+EOF
+  fi
+
+  if [ -z ${POSTFIX_SSL_IN_CERT+x} ]; then
+    POSTFIX_SSL_IN_CERT="/etc/postfix/tls/bundle.crt"
+  fi
+
+  if [ -z ${POSTFIX_SSL_IN_KEY+x} ]; then
+    POSTFIX_SSL_IN_KEY="/etc/postfix/tls/cert.key"
+  fi
+
+  if [ -z ${POSTFIX_SSL_IN_SECURITY_LEVEL+x} ]; then
+    POSTFIX_SSL_IN_SECURITY_LEVEL="may"
+  fi
+
+  if [[ ! -f "$POSTFIX_SSL_IN_CERT" || ! -f "$POSTFIX_SSL_IN_KEY" ]]; then
+    echo ">> POSTFIX SSL - generating incoming self signed ssl cert"
+    openssl req -new -x509 -days 3650 -nodes \
+      -newkey rsa:4096 \
+      -subj "/C=XX/ST=XXXX/L=XXXX/O=XXXX/CN=$MAIL_FQDN" \
+      -out "$POSTFIX_SSL_IN_CERT" \
+      -keyout "$POSTFIX_SSL_IN_KEY" \
+      -sha256
+  fi
+
+  if [[ -f "$POSTFIX_SSL_IN_CERT" && -f "$POSTFIX_SSL_IN_KEY" ]]; then
+    echo ">> POSTFIX SSL - enabling incoming SSL"
+cat <<EOF >> /etc/postfix/main.cf
+### incoming connections ###
+# smtpd_tls_security_level=encrypt # for secure connections only
+smtpd_tls_security_level=$POSTFIX_SSL_IN_SECURITY_LEVEL
+smtpd_tls_cert_file=$POSTFIX_SSL_IN_CERT
+smtpd_tls_key_file=$POSTFIX_SSL_IN_KEY
+
+smtpd_tls_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtpd_tls_mandatory_exclude_ciphers = aNULL, DES, RC4, MD5, 3DES
+smtpd_tls_protocols = !SSLv2 !SSLv3
+smtpd_tls_mandatory_protocols = !SSLv2, !SSLv3
+smtpd_tls_mandatory_ciphers=high
+
+smtpd_tls_session_cache_database = btree:\${data_directory}/smtpd_scache
+smtpd_tls_loglevel = 1
+EOF
+  fi
+
+  if [ -f /etc/postfix/tls/rootCA.crt ]; then
+    echo ">> POSTFIX SSL - enabling CA based Client Authentication"
+    postconf -e smtpd_tls_ask_ccert=yes
+    postconf -e smtpd_tls_CAfile=/etc/postfix/tls/rootCA.crt
+    postconf -e smtpd_recipient_restrictions=permit_mynetworks,permit_sasl_authenticated,permit_tls_all_clientcerts,reject_unauth_destination
+  fi
+
+  if [ -f /etc/postfix/tls/dh1024.pem ]; then
+    echo ">> using dh1024.pem provided in volume"
+    postconf -e 'smtpd_tls_dh1024_param_file=/etc/postfix/tls/dh1024.pem'
+  fi
+
+  if [ -f /etc/postfix/tls/dh512.pem ]; then
+    echo ">> using dh512.pem provided in volume"
+    postconf -e 'smtpd_tls_dh512_param_file=/etc/postfix/tls/dh512.pem'
+  fi
+
+  #
+  # Settings
+  #
+
+  if [ ! -z ${POSTFIX_QUEUE_LIFETIME_BOUNCE+x} ]; then
+    echo ">> POSTFIX set bounce_queue_lifetime = $POSTFIX_QUEUE_LIFETIME_BOUNCE"
+    postconf -e "bounce_queue_lifetime=$POSTFIX_QUEUE_LIFETIME_BOUNCE"
+  fi
+
+  if [ ! -z ${POSTFIX_QUEUE_LIFETIME_MAX+x} ]; then
+    echo ">> POSTFIX set maximal_queue_lifetime = $POSTFIX_QUEUE_LIFETIME_MAX"
+    postconf -e "maximal_queue_lifetime=$POSTFIX_QUEUE_LIFETIME_MAX"
+  fi
+
+  if [ ! -z ${POSTFIX_MYDESTINATION+x} ]; then
+    echo ">> POSTFIX set mydestination = $POSTFIX_MYDESTINATION"
+    postconf -e "mydestination=$POSTFIX_MYDESTINATION"
+  fi
+
+  if [ -f /etc/postfix/additional/transport ]; then
+    echo ">> POSTFIX found 'additional/transport' activating it as transport_maps"
+    postmap /etc/postfix/additional/transport
+    postconf -e "transport_maps = hash:/etc/postfix/additional/transport"
+  fi
+
+  if [ -f /etc/postfix/additional/header_checks ]; then
+    echo ">> POSTFIX found 'additional/header_checks' activating it as header_checks"
+    postconf -e "header_checks = regexp:/etc/postfix/additional/header_checks"
+  fi
+
+  ##
+  # SQL Statements
+  ##
+
+  if [ -z ${SQL_VIRTUAL_MAILBOX_DOMAIN+x} ]; then
+    SQL_VIRTUAL_MAILBOX_DOMAIN="SELECT 1 FROM virtual_domains WHERE name='%s'"
+  fi
+  echo ">> SQL_VIRTUAL_MAILBOX_DOMAIN: $SQL_VIRTUAL_MAILBOX_DOMAIN"
+  echo "query = $SQL_VIRTUAL_MAILBOX_DOMAIN" >> /etc/postfix/mysql-virtual-mailbox-domains.cf
+
+  if [ -z ${SQL_VIRTUAL_MAILBOX_MAPS+x} ]; then
+    SQL_VIRTUAL_MAILBOX_MAPS="SELECT 1 FROM virtual_users WHERE email='%s'"
+  fi
+  echo ">> SQL_VIRTUAL_MAILBOX_MAPS: $SQL_VIRTUAL_MAILBOX_MAPS"
+  echo "query = $SQL_VIRTUAL_MAILBOX_MAPS" >> /etc/postfix/mysql-virtual-mailbox-maps.cf
+
+  if [ -z ${SQL_VIRTUAL_ALIAS_MAPS+x} ]; then
+    SQL_VIRTUAL_ALIAS_MAPS="SELECT destination FROM virtual_aliases WHERE source='%s'"
+  fi
+  echo ">> SQL_VIRTUAL_ALIAS_MAPS: $SQL_VIRTUAL_ALIAS_MAPS"
+  echo "query = $SQL_VIRTUAL_ALIAS_MAPS" >> /etc/postfix/mysql-virtual-alias-maps.cf
+
+  if [ -z ${SQL_EMAIL_TO_EMAIL+x} ]; then
+    SQL_EMAIL_TO_EMAIL="SELECT email FROM virtual_users WHERE email='%s'"
+  fi
+  echo ">> SQL_EMAIL_TO_EMAIL: $SQL_EMAIL_TO_EMAIL"
+  echo "query = $SQL_EMAIL_TO_EMAIL" >> /etc/postfix/mysql-email2email.cf
+
+  if [ -z ${SQL_DOVECOT_PASSWORD_QUERY+x} ]; then
+    SQL_DOVECOT_PASSWORD_QUERY="SELECT email as user, password FROM virtual_users WHERE email='%u';"
+  fi
+  echo ">> SQL_DOVECOT_PASSWORD_QUERY: $SQL_DOVECOT_PASSWORD_QUERY"
+  echo "password_query = $SQL_DOVECOT_PASSWORD_QUERY" >> /etc/dovecot/dovecot-sql.conf.ext
+
+  ##
+  # POSTFIX RAW Config ENVs
+  ##
+  if env | grep '^POSTFIX_RAW_CONFIG_'
+  then
+    echo -e "\n## POSTFIX_RAW_CONFIG ##\n" >> /etc/postfix/main.cf
+    for I_CONF in "$(env | grep '^POSTFIX_RAW_CONFIG_')"
+    do
+      CONFD_CONF_NAME=$(echo "$I_CONF" | cut -d'=' -f1 | sed 's/POSTFIX_RAW_CONFIG_//g' | tr '[:upper:]' '[:lower:]')
+      CONFD_CONF_VALUE=$(echo "$I_CONF" | sed 's/^[^=]*=//g')
+
+      echo "$CONFD_CONF_VALUE" >> /etc/postfix/main.cf
+    done
+  fi
+
+  ###
+  # RUNIT
+  ###
+
+  echo ">> RUNIT - create services"
+  mkdir -p /etc/sv/rsyslog /etc/sv/postfix /etc/sv/dovecot
+  echo -e '#!/bin/sh\nexec /usr/sbin/rsyslogd -n' > /etc/sv/rsyslog/run
+    echo -e '#!/bin/sh\nrm /var/run/rsyslogd.pid' > /etc/sv/rsyslog/finish
+  echo -e '#!/bin/sh\n/usr/sbin/postfix start; sleep 5; while ps aux | grep [p]ostfix | grep [m]aster > /dev/null 2> /dev/null; do sleep 5; done' > /etc/sv/postfix/run
+    echo -e '#!/bin/sh\n/usr/sbin/postfix stop' > /etc/sv/postfix/finish
+  echo -e '#!/bin/sh\nexec /usr/sbin/dovecot -F | logger -t dovecot' > /etc/sv/dovecot/run
+  chmod a+x /etc/sv/*/run /etc/sv/*/finish
+
+  echo ">> RUNIT - enable services"
+  ln -s /etc/sv/dovecot /etc/service/dovecot
+  ln -s /etc/sv/postfix /etc/service/postfix
+  ln -s /etc/sv/rsyslog /etc/service/rsyslog
+
+fi
+
+echo ">> fix file permissions"
+chgrp postfix /etc/postfix/m*.cf
+chmod u=rw,g=r,o= /etc/postfix/m*.cf
+chgrp vmail /etc/dovecot/dovecot.conf
+chmod g+r /etc/dovecot/dovecot.conf
+chown root:root /etc/dovecot/dovecot-sql.conf.ext
+chmod go= /etc/dovecot/dovecot-sql.conf.ext
+
+echo ">> starting services"
+exec runsvdir -P /etc/service

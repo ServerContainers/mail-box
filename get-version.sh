7c8c60ade7dea4a6f6801b2b1e3bb1e31ca5cc26
Marvin Winkler <m@rvin.at>
2023-03-22 09:58:15 +0100

    small fix

--- get-version.sh
+++ get-version.sh
@@ -2,11 +2,11 @@
 export IMG=$(docker build -q --pull --no-cache -t 'get-version' .)
 
 export DEBIAN_VERSION=$(docker run --rm -t get-version cat /etc/debian_version | tail -n1 | tr -d '\r')
-export POSTFIX_VERSION=$(docker run --rm -t get-version dpkg --list postfix | grep '^ii' | sed 's/^[^0-9]*//g' | cut -d ' ' -f1 | sed 's/[+=]/_/g' | tr -d '\r')
-export DOVECOT_VERSION=$(docker run --rm -t get-version dpkg --list dovecot-core | grep '^ii' | sed 's/^[^0-9]*//g' | cut -d ' ' -f1 | sed 's/[+=]/_/g' | tr -d '\r')
+export POSTFIX_VERSION=$(docker run --rm -t get-version dpkg --list postfix | grep '^ii' | sed 's/^[^0-9]*//g' | cut -d ' ' -f1 | sed 's/[:+=]/_/g' | tr -d '\r')
+export DOVECOT_VERSION=$(docker run --rm -t get-version dpkg --list dovecot-core | grep '^ii' | sed 's/^[^0-9]*//g' | cut -d ' ' -f1 | sed 's/[:+=]/_/g' | tr -d '\r')
 [ -z "$DEBIAN_VERSION" ] && exit 1
 
-export IMGTAG=$(echo "$1""d$DEBIAN_VERSION-p$POSTFIX_VERSION-d$DOVECOT_VERSION")
+export IMGTAG=$(echo "$1""d$DEBIAN_VERSION-p$POSTFIX_VERSION-dv$DOVECOT_VERSION")
 export IMAGE_EXISTS=$(docker pull "$IMGTAG" 2>/dev/null >/dev/null; echo $?)
 
 # return latest, if container is already available :)
